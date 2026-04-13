33a5d2a672c51d723f3333d0b30b812f5b1bfd3a
Raphaël Le Bidan <raphael.lebidan@imt-atlantique.fr>
2026-04-13 11:26:02 +0200

    feat: add crossmpt

--- src/crossmpt.py
+++ src/crossmpt.py
@@ -1,13 +1,11 @@
-# Error-Correction Code Transformer model from https://arxiv.org/abs/2206.14881
-# Most of this code is taken from https://github.com/yoniLc/ECCT
-# and was released under MIT license. Copyright (c) 2022 yoniLc
+# Cross Message-Passing Transformer model from https://arxiv.org/abs/2507.01038
+# Most of this code is taken from https://github.com/iil-postech/crossmpt 
+# and was released for non-commercial research purpose by Seong-Joon Park
 
 # Main changes with respect to the original implementation:
 # - use of pytorch's scaled_dot_product_attention
-# - mask modified to prevent tokens to attend to themselves
 # - disable bias in FFN layers
 # - no more intermediate layernorm in the middle
-# - syndromes in binary rather than bipolar form
 
 import copy, math
 import torch, torch.nn as nn, torch.nn.functional as F
@@ -30,10 +28,11 @@ class Encoder(nn.Module):
         self.layers = clones(layer, N)
         self.norm = LayerNorm(layer.size)
 
-    def forward(self, x: Tensor, mask: Tensor) -> Tensor:
+    def forward(self, x1: Tensor, x2: Tensor, mask_VN: Tensor, mask_CN: Tensor) -> Tensor:
         for idx, layer in enumerate(self.layers, start=1):
-            x = layer(x, mask)
-        return self.norm(x)
+            x1 = layer(x1, x2, mask_VN)
+            x2 = layer(x2, x1, mask_CN)
+        return self.norm(x1), self.norm(x2)
 
 
 class SublayerConnection(nn.Module):
@@ -60,9 +59,9 @@ class EncoderLayer(nn.Module):
         self.sublayer = clones(SublayerConnection(size, dropout), 2)
         self.size = size
 
-    def forward(self, x: Tensor, mask: Tensor) -> Tensor:
-        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
-        return self.sublayer[1](x, self.feed_forward)
+    def forward(self, x1: Tensor, x2: Tensor, mask: Tensor) -> Tensor:
+        x1 = self.sublayer[0](x1, lambda x: self.self_attn(x, x2, x2, mask))
+        return self.sublayer[1](x1, self.feed_forward)
 
 
 class MultiHeadedAttention(nn.Module):
@@ -127,7 +126,7 @@ class PositionwiseFeedForward(nn.Module):
         return self.w_2(self.dropout(F.gelu(self.w_1(x))))
 
 
-class ECC_Transformer(nn.Module):
+class CrossMPT(nn.Module):
     def __init__(
         self,
         code: LinearCode,
@@ -147,7 +146,7 @@ class ECC_Transformer(nn.Module):
         N_dec, d_model, h = n_layers, embed_dim, n_heads
         output_sz = code.k if output == "message" else code.n
 
-        log.info(f"Building a {n_layers}-layer ECCT decoder")
+        log.info(f"Building a {n_layers}-layer CrossMPT decoder")
         log.info(f"Embedding dimension = {embed_dim}")
         log.info(
             f"Self-attention uses {n_heads} heads of dimension {embed_dim // n_heads}"
@@ -162,7 +161,8 @@ class ECC_Transformer(nn.Module):
         c = copy.deepcopy
         attn = MultiHeadedAttention(h, d_model, attn_dropout, use_fast_attn)
         ff = PositionwiseFeedForward(d_model, d_model * up_proj, ffn_dropout)
-        self.src_embed = torch.nn.Parameter(torch.empty((code.n + code.m, d_model)))
+        self.src_embed_VN = torch.nn.Parameter(torch.empty((code.n, d_model)))
+        self.src_embed_CN = torch.nn.Parameter(torch.empty((code.m, d_model)))
         self.decoder = Encoder(
             EncoderLayer(d_model, c(attn), c(ff), res_dropout), N_dec
         )
@@ -173,9 +173,9 @@ class ECC_Transformer(nn.Module):
             log.info("Compiling model layers for faster training")
             self.decoder.compile()
 
-        # setup mask and other parameters
+        # setup masks and other parameters
 
-        self.get_mask(code, use_fast_attn)
+        self.get_masks(code, use_fast_attn)
 
         for p in self.parameters():
             if p.dim() > 1:
@@ -186,36 +186,43 @@ class ECC_Transformer(nn.Module):
         )  # for model summary
 
     def forward(self, ym: Tensor, s: Tensor) -> Tensor:
-        x = torch.cat([ym, (1 - s) / 2], dim=1)  # (|y|, s) with s in binary format
-        emb = x.unsqueeze(-1)
-        emb = self.src_embed.unsqueeze(0) * emb
-        emb = self.decoder(emb, self.src_mask)
+        VN = ym.unsqueeze(-1) * self.src_embed_VN.unsqueeze(0)
+        CN = s.unsqueeze(-1) * self.src_embed_CN.unsqueeze(0)
+        emb1, emb2 = self.decoder(VN, CN, self.src_mask_VN, self.src_mask_CN)
+        emb = torch.cat([emb1, emb2], dim=1)
         return self.out_fc(self.oned_final_embed(emb).squeeze(-1))
 
-    def get_mask(self, code: LinearCode, invert: bool = False) -> None:
+    @staticmethod
+    def _enforce_stride1_in_last_dim(x: Tensor) -> Tensor:
+        # solution from https://github.com/pytorch/pytorch/issues/127523
+        if x.stride(-1) != 1:
+            x = torch.empty_like(x, memory_format=torch.contiguous_format).copy_(x)
+        return x    
 
-        def build_mask(code: LinearCode, invert: bool = False) -> Tensor:
-            mask_size = code.n + code.m
-            # MODIF RLB: modify mask to prevent tokens from attending to themselves
-            mask = torch.zeros(
-                mask_size, mask_size
-            )  # torch.eye(mask_size, mask_size) in the original implementation
-            for ii in range(code.m):
-                idx = torch.where(code.H[ii] > 0)[0]
-                for jj in idx:
-                    for kk in idx:
-                        if jj != kk:
-                            mask[jj, kk] += 1
-                            mask[kk, jj] += 1
-                            mask[code.n + ii, jj] += 1
-                            mask[jj, code.n + ii] += 1
-            # mask=True for elements that do NOT participate to attention
-            # we need to invert the mask for Pytorch SDPA (opposite convention)
-            src_mask = ~(mask > 0)
-            return ~src_mask if invert else src_mask
+    def get_masks(self, code: LinearCode, invert: bool = False) -> None:
+ 
+        def build_mask(code: LinearCode, invert: bool = False):
+            mask = code.H > 0
+            # Original code: mask=True for elements that DON'T participate to attention
+            # Need to invert for Pytorch SDPA: mask=True for elements that DO participate to attention
+            src_mask = mask if invert else ~mask 
+            return src_mask
+        
+        src_mask_CN = build_mask(code, invert=invert)
+        src_mask_VN = src_mask_CN.T
 
-        src_mask = build_mask(code, invert=invert)
-        self.register_buffer("src_mask", src_mask)
+        # make sure last dim of each mask has stride=1, as this is what fast attention and 
+        # memory-efficient attention expect for *all* of their input tensors, mask included
+        # see: https://github.com/pytorch/pytorch/issues/116333        
+        src_mask_CN = CrossMPT._enforce_stride1_in_last_dim(src_mask_CN)
+        src_mask_VN = CrossMPT._enforce_stride1_in_last_dim(src_mask_VN)
+        assert src_mask_CN.stride(-1) == 1, \
+            f"The last dim of src_mask_CN must have stride 1 (got {src_mask_CN.stride(-1)})"
+        assert src_mask_VN.stride(-1) == 1, \
+            f"The last dim of src_mask_VN must have stride 1 (got {src_mask_VN.stride(-1)})"
+        
+        self.register_buffer("src_mask_VN", src_mask_VN)
+        self.register_buffer("src_mask_CN", src_mask_CN)
 
 
 if __name__ == "__main__":
